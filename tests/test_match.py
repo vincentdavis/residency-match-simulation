370e3e6c654ef638ec5127a014f8a55e7ece0361
Vincent Davis <vincent@heteroskedastic.com>
2016-02-26 11:10:18 -0700

    match tests not passing.

--- tests/test_match.py
+++ tests/test_match.py
@@ -5,11 +5,12 @@ from match import Match
 
 def setup_app():
     applist=[]
-    for qualty in [100,20,90,80,50,30,40,10,70,60]:
+    for qualty in [100,20,90,80,50,30,40,10,70,60, 25,95,85,55,35, 45, 15, 75, 65, 15, 10, 5, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
         name = App()
         name.quality = qualty
         name.observed_1 = .8
         name.observed_2 = 1.25
+        name.applied_to = 3
         name.name = 'a'+str(qualty)
         applist.append(name)
     return applist
@@ -21,6 +22,8 @@ def setup_inst():
         name.quality = qualty
         name.observed_1 = .8
         name.observed_2 = 1.25
+        name.number_to_interview = 5
+        name.openings = 2
         name.name = 'i'+str(qualty)
         instlist.append(name)
     return instlist
@@ -28,11 +31,12 @@ def setup_inst():
 class TestMatch(unittest.TestCase):
     
     def test_Applicant_not_matched(self):
-        amatch=Match(setup_app, setup_inst)
+        amatch=Match(setup_app(), setup_inst()).Applicant_not_matched()
+        self.assertEqual(amatch, 1, [i.name for i in amatch])
         
         
     def test_run_match(self):
-        amatch=Match(setup_app, setup_ins)
-      
+        amatch=Match(setup_app(), setup_inst())
+        self.assertEqual(amatch, 1, [i.name for i in amatch.app_list])
 if __name__ == "__main__":
     unittest.main()
