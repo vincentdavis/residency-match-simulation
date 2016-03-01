883c296a2ee9fc761b68cf566fefa109a9a02294
Vincent Davis <vincent@vincentdavis.net>
2016-02-29 18:52:50 -0700

    update git

---
+++ models.py
@@ -0,0 +1,31 @@
+
+from peewee import *
+
+database = SqliteDatabase('match.db')
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+class Applicant(BaseModel):
+    name = CharField(null=False)
+    quality = FloatField(null=False)
+    apply = FloatField(null=False)
+    visit = FloatField(null=False)
+    marketing = FloatField(null=False)
+    applylimit = IntegerField(null=True)
+
+class Institution(BaseModel):
+    name = CharField(null=False)
+    quality = FloatField(null=False)
+    application = FloatField(null=False)
+    interview = FloatField(null=False)
+    marketing = FloatField(null=False)
+    interviewlimit = IntegerField(null=True)
+
+class Match(BaseModel):
+    app = ForeignKeyField(Applicant)
+    inst = ForeignKeyField(Institution)
+    app_rank_inst = IntegerField(null=True)
+    inst_rank_app = IntegerField(null=True)
+    matched = IntegerField(default=-1)
