580e02057cb25499626ba16662966b96061d084f
Vincent Davis <vincent@vincentdavis.net>
2016-03-01 17:47:41 -0700

    update basic test, expand match model

--- tests/test_perfect.py
+++ tests/test_perfect.py
@@ -12,27 +12,27 @@ class testperfect_1(TestCase):
         database.create_tables([Applicant, Institution, Match])
 
     def create_applicants(applicant):
-        count = 5000
-        for a in range(5000):
+        count = 500
+        for a in range(count):
             app = applicant.create(name='A'+str(a+1), quality=(100/count)*(a+1),
                                    apply=1, visit=1, marketing=1, applylimit=100)
 
     def create_intitution(institution):
-        count = 100
-        for a in range(5000):
+        count = 10
+        for a in range(count):
             app = institution.create(name='I'+str(a+1), quality=(100/count)*(a+1),
                                    application=1, interview=1, marketing=1, interviewlimit=5000)
 
     def request_interview(applicant, institution):
         for a in applicant:
             for i in institution:
-                Match.create(inst=i, app=a, matched=-1)
+                Match.create(inst=i, app=a, req_interview=1, matched=-1)
 
-
-        pass
-
-    def invite_interview (self):
-        pass
+    def invite_interview (match):
+        for m in match:
+            if m.req_interview==1: # this should be everyone.
+                m.inst_interview = 1
+                m.save()
 
     def rank_inst(self):
         pass
@@ -42,3 +42,6 @@ class testperfect_1(TestCase):
     create_applicants(Applicant)
     create_intitution(Institution)
     request_interview(Applicant, Institution)
+    invite_interview (Match)
+
+
